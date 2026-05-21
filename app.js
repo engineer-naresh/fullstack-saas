bb7bd6fa8c9cc34a33ad8d1e7ef3bc242a3ba302
engineer-naresh <nareshcodes@gmail.com>
2026-05-21 20:31:52 +0545

    book post api

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express =  require("express");
+const { books, users } = require("./database/connection");
 const app = express();
 require("./database/connection");
-app.get('/books',(request,response)=>{
+app.use(express.json());
+app.get('/books',async(request,response)=>{
     //code logic to retrieve books goes here..
-
+   const datas = await books.findAll(); //findAll() always returns array
     response.json({
-        message:"All books shown here"
+        message:"All books shown here",
+        datas
     });
 });
-app.post('/books',(req,res)=>{
+app.post('/books',async(req,res)=>{
     //code logic to add books goes here
+    const {bookName,bookAuthor,bookGenre,bookPrice} =req.body
+    await books.create({
+        bookName:bookName,
+        bookAuthor:bookAuthor,
+        bookGenre:bookGenre,
+        bookPrice:bookPrice
+    })
+    console.log(bookName,bookAuthor,bookGenre,bookPrice)
+
     res.json({
     message:"Books added successfully!"
 })
@@ -31,6 +43,13 @@ app.delete("/books/:id",(req,res)=>{
 });
 // DATABASE_URL="postgresql://postgres.rvqosousksvaocazuqnm:[YOUR-PASSWORD]@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
 
+app.get('/users',async(request,response)=>{
+    const userData= await users.findAll()
+    response.json({
+        message:"Users shown here",
+        userData
+    })
+})
 
 app.listen(3000,()=>{
     console.log("server/backend has started at port 3000");
