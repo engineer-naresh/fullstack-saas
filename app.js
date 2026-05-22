466721e05ef519627e6fe8e7217632718c7058ff
engineer-naresh <nareshcodes@gmail.com>
2026-05-22 10:44:58 +0545

    mvc refactoring

--- app.js
+++ app.js
@@ -1,47 +1,16 @@
 const express =  require("express");
 const { books, users } = require("./database/connection");
+const {fetchBooks,addBooks,updateBook, deleteBook} = require("./controllers/book.controller");
 const app = express();
 require("./database/connection");
 app.use(express.json());
-app.get('/books',async(request,response)=>{
-    //code logic to retrieve books goes here..
-   const datas = await books.findAll(); //findAll() always returns array
-    response.json({
-        message:"All books shown here",
-        datas
-    });
-});
-app.post('/books',async(req,res)=>{
-    //code logic to add books goes here
-    const {bookName,bookAuthor,bookGenre,bookPrice} =req.body
-    await books.create({
-        bookName:bookName,
-        bookAuthor:bookAuthor,
-        bookGenre:bookGenre,
-        bookPrice:bookPrice
-    })
-    console.log(bookName,bookAuthor,bookGenre,bookPrice)
+app.get('/books',fetchBooks);
+app.post('/books',addBooks);
 
-    res.json({
-    message:"Books added successfully!"
-})
-});
-
-app.patch("/books/:id",(req,res)=>{
-    //code to update books goes here
-    res.json({
-        message:"Books updated successfully!"
-    });
-});
+app.patch("/books/:id",updateBook);
 
 
-app.delete("/books/:id",(req,res)=>{
-    //code to delete books goes here
-    res.json({
-        message:"Books deleted successfully!"
-    });     
-});
-// DATABASE_URL="postgresql://postgres.rvqosousksvaocazuqnm:[YOUR-PASSWORD]@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
+app.delete("/books/:id",deleteBook);
 
 app.get('/users',async(request,response)=>{
     const userData= await users.findAll()
---
+++ controllers/book.controller.js
@@ -0,0 +1,38 @@
+const { books } = require("../database/connection");
+
+const fetchBooks = async(request,response)=>{
+    //code logic to retrieve books goes here..
+   const datas = await books.findAll(); //findAll() always returns array
+    response.json({
+        message:"All books shown here",
+        datas
+    });
+};
+const addBooks = async(req,res)=>{
+    //code logic to add books goes here
+    const {bookName,bookAuthor,bookGenre,bookPrice} =req.body
+    
+    await books.create({
+        bookName,
+        bookAuthor,
+        bookGenre,
+        bookPrice
+    })
+    console.log(req.body);
+    res.json({
+    message:"Books added successfully!"
+})
+}
+const updateBook = (req,res)=>{
+    //code to update books goes here
+    res.json({
+        message:"Books updated successfully!"
+    });
+}
+const deleteBook = (req,res)=>{
+    //code to delete books goes here
+    res.json({
+        message:"Books deleted successfully!"
+    });     
+}
+module.exports = {fetchBooks, addBooks, updateBook, deleteBook};
